0238a0c3d5f56acb668777d9991e4ed5d1e3655a
Guanghui Qin <hiaoxui@gmail.com>
2023-12-24 22:20:03 +0100

    make it a package & load data from disk

--- dlutils/__init__.py
+++ dlutils/__init__.py
@@ -1,4 +1,4 @@
 from .suppress_warnings import suppress
-from .cli import process_args, Default, add_gpu_arguments
+from .cli import process_args, Default, add_gpu_arguments, add_common_args
 from .common import logger, param_names
 from .param import param_to_buffer
--- dlutils/lazy_dataset.py
+++ dlutils/lazy_dataset.py
@@ -1,11 +1,12 @@
 from typing import *
+import os
 
 from torch.utils import data
 
 
 class BaseLazyDataset:
     def __init__(
-            self, data_args: Optional[Iterable] = None, split: Optional[str] = None,
+            self, data_args: Iterable = None, split: Optional[str] = None,
     ):
         self._data_args, self._split = data_args, split
         from datasets import DatasetDict
@@ -24,10 +25,21 @@ class BaseLazyDataset:
     def clean(self):
         self._data = None
 
+    def _get_data(self):
+        from datasets import load_dataset, load_from_disk
+        if self._data is not None:
+            return self._data
+        if os.path.exists(self._data_args[0]):
+            args = list(self._data_args)
+            if self._split is not None:
+                args[0] = os.path.join(args[0], self._split)
+            return load_from_disk(*args)
+        else:
+            return load_dataset(*self._data_args, split=self._split)
+
     def _prepare_data(self):
-        from datasets import load_dataset
         if self._data_args is not None and self._data is None:
-            self._data = load_dataset(*self._data_args, split=self._split)
+            self._data = self._get_data()
 
 
 class IterableLazyDataset(BaseLazyDataset, data.IterableDataset):
@@ -43,4 +55,13 @@ class LazyDataset(BaseLazyDataset, data.Dataset):
         self._data_length = data_length
 
     def __len__(self):
-        return self._data_length
+        if self._data_length is not None:
+            if self._data is not None:
+                return min(len(self._data), self._data_length)
+            return self._data_length
+        elif self._data is not None:
+            return len(self._data)
+        else:
+            tmp_data = self._get_data()
+            self._data_length = len(tmp_data)
+            return self._data_length
