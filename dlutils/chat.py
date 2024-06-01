8aafb547239b8535a72ba1f77d878a8fc386f60d
Guanghui Qin <hiaoxui@gmail.com>
2024-06-01 19:11:10 -0400

    split point

--- dlutils/chat.py
+++ dlutils/chat.py
@@ -1,9 +1,17 @@
-from typing import List
+from typing import List, Dict
 from dataclasses import dataclass, field
 from copy import deepcopy
 import re
 
 from .lazy_tokenizer import LazyTokenizer
+
+
+@dataclass
+class ChatReturn:
+    input_ids: List[int]
+    prefix: int | None = None
+    chats: List[Dict[str, str]] | None = None
+    chat_str: str | None = None
 
 
 @dataclass
@@ -14,6 +22,8 @@ class ChatPart:
     min_tokens: int = 0
     # parts with higher priority will be first truncated
     truncate_priority: int = 0
+    # If this is the start of the training part of the sequence
+    prefix_split: bool = False
     # below are fields used by factory; do not specify them yourself
     ids: List[int] | None = None
     # uuid: UUID = field(default_factory=uuid4)
@@ -47,8 +57,6 @@ class Chat:
 @dataclass
 class ChatInput:
     chats: List[Chat] = field(default_factory=list)
-    # used by factory
-    ids: List[int] = field(default_factory=list)
 
     def __repr__(self):
         return f'<ChatInput: {len(self.chats)} chats>'
@@ -62,6 +70,12 @@ class ChatInput:
                 mapping[(i, j)] = part
                 fake_content += f'∇{i},{j}∇'
             fake_chat.append({'role': chat.role, 'content': fake_content})
+    
+    def tolist(self):
+        return [
+            {'role': chat.role, 'content': ''.join([part.content for part in chat.parts])}
+            for chat in self.chats
+        ]
 
 
 class ChatFactory(LazyTokenizer):
@@ -70,7 +84,7 @@ class ChatFactory(LazyTokenizer):
         self.max_tokens = max_tokens
         self.tokenizer_kwargs = {'legacy': False, 'use_fast': True}
     
-    def process(self, chat_input: ChatInput, tokenize: bool):
+    def process(self, chat_input: ChatInput, return_text: bool) -> ChatReturn:
         chat_input = deepcopy(chat_input)
         trun_parts = []
         for chat in chat_input.chats:
@@ -93,8 +107,8 @@ class ChatFactory(LazyTokenizer):
             fake_chat.append({'role': chat.role, 'content': fake_content})
         template = self.remove_bos_eos(self.tokenizer.apply_chat_template(fake_chat, tokenize=False))
 
-        pieces = []
-        re_pat = re.compile(r'(∇(\d+),(\d+)∇)')
+        pieces: List[ChatPart] = []
+        re_pat = re.compile(r'(∇(\d+),(\d+)∇)', flags=re.MULTILINE)
         while template:
             matches = re_pat.findall(template)
             if matches:
@@ -102,22 +116,38 @@ class ChatFactory(LazyTokenizer):
                 i, j = map(int, first[1:])
                 left = template.index(first[0])
                 if left != 0:
-                    pieces.append(self.tok(template[:left]))
+                    pieces.append(ChatPart(template[:left], ids=self.tok(template[:left])))
                 template = template[left + len(first[0]):]
-                pieces.append(mapping[(i, j)].get_ids)
+                pieces.append(mapping[(i, j)])
             else:
-                pieces.append(self.tok(template))
+                pieces.append(ChatPart(template, ids=self.tok(template)))
                 break
         
         def construct_input_ids():
-            return sum([piece if isinstance(piece, list) else piece() for piece in pieces], start=[])
+            return sum([piece.get_ids() for piece in pieces], start=[])
 
+        # truncation
         remove_left = len(construct_input_ids()) - (self.max_tokens - 2)
         truncate_idx = 0
         while remove_left > 2:
             if truncate_idx >= len(trun_parts):
                 raise NotImplementedError
             remove_left -= trun_parts[truncate_idx].try_to_truncate(remove_left)
+            if return_text:
+                part.content = self.tokenizer.decode(part.ids)
         
-        final_ids = construct_input_ids()
-        return self.add_bos_eos_ids(final_ids)
+        ret = ChatReturn(construct_input_ids(), prefix=self.detect_prefix(pieces=pieces))
+        if return_text:
+            ret.chats = chat_input.tolist()
+            ret.chat_str = self.tokenizer.decode(ret.input_ids)
+
+        return ret
+    
+    @staticmethod
+    def detect_prefix(pieces: List[ChatPart]) -> int | None:
+        prefix_idx = -1
+        for pie in pieces:
+            if pie.prefix_split:
+                return prefix_idx
+            prefix_idx += len(pie.ids)
+        return
